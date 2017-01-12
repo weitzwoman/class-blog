b4d409317052303134f2e253ff6da957a34e4516
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-12 13:03:09 -0800

    working on adding comments

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,9 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  author: DS.attr(),
   date: DS.attr(),
-  photo: DS.attr(),
-  title: DS.attr(),
   content: DS.attr(),
-  comments: DS.hasMany('comment', { async: true })
+  post: DS.belongsTo('post', { async: true })
 });
---
+++ app/routes/post.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('post', params.post_id);
+  },
+  actions: {
+    saveComment(params) {
+      var newComment = this.store.createRecord('comment', params);
+      var post = params.post;
+      post.get('commments').addObject(newComment);
+      newComment.save().then(function() {
+        return post.save();
+      });
+      this.transitionTo('post', post);
+    }
+  }
+});
